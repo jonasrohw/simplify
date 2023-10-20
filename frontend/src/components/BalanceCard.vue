5dbd940d5ee04ccc8c410df27a68ba5760270dbb
lucordes <cordes135@gmail.com>
2023-10-20 20:32:35 +0200

    add incoming outgoing transaction buttons

--- frontend/src/components/BalanceCard.vue
+++ frontend/src/components/BalanceCard.vue
@@ -6,10 +6,14 @@ h1 {
 </style>
 
 <template>
-  <main class="w-full mx-auto bg-gradient-to-r from-amber-500 to-amber-400 ring-2 ring-amber-600 px-1 py-1 rounded-xl flex gap-4 items-start justify-evenly">
+  <main class="w-full mx-auto bg-gradient-to-r from-amber-500 to-amber-400 ring-0 ring-amber-600 px-1 py-5 rounded-xl flex gap-4 items-start justify-evenly">
+	<p><i class="fa fa-arrow-left" style="font-size:60px;"></i><br>
+		Outgoing</p>
+	
 		<section>
-			<p class="text-lg">Balance</p>
-			<h1>{{ balances.current.toLocaleString() }} EUR</h1>
+			<!-- <p class="text-lg">Balance</p> -->
+			<h1>IBAN XXXXXXXXXXXXX</h1>
+			<h1 style="font-size:30px;">{{ balances.current.toLocaleString() }} €</h1>
 			<!-- <span class="w-full flex justify-between items-center">
 				<small>{{ balances.trend.value.toLocaleString() }} EUR</small>
 				<span class="flex items-end">
@@ -18,6 +22,8 @@ h1 {
 				</span>
 			</span> -->
 		</section>
+		<p><i class="fa fa-arrow-right" style="font-size:60px;"></i><br>
+		Incoming</p>
 		<!-- <section class="grid place-items-center rounded-lg bg-gradient-to-r from-amber-500 to-amber-500 shadow shadow-gray-800 shadow-inner text-gray-300 px-2 py-3">
 			<section class="flex flex-col text-center">
 				<span class="text-sm">Your points</span>
