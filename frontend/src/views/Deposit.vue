7f6effaae96bb086d25ca934f240b793b3c9a07c
yccyangchen <1193657480@qq.com>
2023-10-21 10:29:12 +0200

    add slide function

--- frontend/src/views/Deposit.vue
+++ frontend/src/views/Deposit.vue
@@ -1,4 +1,4 @@
-<!-- <style scoped>
+<style scoped>
 
   /* .input-wrapper {
     @apply flex flex-col h-20 text-gray-300;
@@ -11,12 +11,12 @@
   }
 </style>
 <template>
-	<div class="fill">
+	<!-- <div class="fill">
 		<img src="/long_story.png" alt="long story 1">
-	</div>
-  <Splide class="mt-3 z-10" :options="options">
+	</div> -->
+  <Splide class="mt-3 z-10" options="options">
     <SplideSlide>
-      <img src="/banner-1.jpg" alt="Sample 1">
+      <img src="/long_story.png" alt="Sample 1">
     </SplideSlide>
     <SplideSlide>
       <img src="/banner-2.jpg" alt="Sample 2">
@@ -27,72 +27,19 @@
     <SplideSlide>
       <img src="/banner-2.jpg" alt="Sample 2">
     </SplideSlide>
-  </Splide> -->
-<!-- </template> -->
+  </Splide>
+ </template>
 
-<template>
-	<div
-	  class="swipe-container"
-	  @touchstart="onTouchStart"
-	  @touchmove="onTouchMove"
-	  @touchend="onTouchEnd"
-	>
-	  <img :src="currentImage" alt="Swipe Image" class="swipe-image" />
-	</div>
-  </template>
-  
-  <script>
-  export default {
-	name: 'SwipeImage',
-	data() {
-	  return {
-		images: [
-		'/banner-1.jpg', // Replace this with your actual image path
-        '/long-story.png', // Replace this with your actual image path
-        '/banner-2.jpg', // Replace this with your actual image path
-      ],
-		currentIndex: 0,
-		startX: 0,
-		endX: 0,
-	  };
-	},
-	computed: {
-	  currentImage() {
-		return this.images[this.currentIndex];
-	  },
-	},
-	methods: {
-	  onTouchStart(event) {
-		this.startX = event.touches[0].clientX;
-	  },
-	  onTouchMove(event) {
-		this.endX = event.touches[0].clientX;
-	  },
-	  onTouchEnd() {
-		if (this.endX - this.startX > 50 && this.currentIndex > 0) {
-		  this.currentIndex--;
-		} else if (this.startX - this.endX > 50 && this.currentIndex < this.images.length - 1) {
-		  this.currentIndex++;
-		}
-		this.startX = 0;
-		this.endX = 0;
-	  },
-	},
-  };
-  </script>
-  
-  <style>
-  .swipe-container {
-	position: relative;
-	width: 100%;
-	height: 300px;
-	overflow: hidden;
-  }
-  
-  .swipe-image {
-	width: 100%;
-	height: 100%;
-	object-fit: cover;
-  }
-  </style>
-  
+<script setup>
+
+import { Splide, SplideSlide } from '@splidejs/vue-splide'
+import '@splidejs/splide/dist/css/splide.min.css';
+
+const options = {
+  arrows: false,
+  autoplay: true,
+  perpage: 1,
+  type: 'loop'
+}
+
+</script>
